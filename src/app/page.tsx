a86381de1239eb4749c55e5a7c56ba0e9ab19d60
yawn <69970183+yawn-c111@users.noreply.github.com>
2025-04-12 20:36:21 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
+        <p>test</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
